03ff022a398fec0a020c41c6dfb0e9ced9827eec
Kirill Miller <belogorski2000@gmail.com>
2022-08-26 10:27:53 +0300

    Добавьте файлы проекта.

---
+++ _8.1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+/*Выберите любую папку на своем компьютере, имеющую вложенные директории. 
+Выведите на консоль ее содержимое и содержимое всех подкаталогов.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\T";
+            string[] files = Directory.GetFiles(path, "", SearchOption.AllDirectories);
+
+            //если ввожу фармат файла ничего не выдает, это не в ходит в поставленную задачу просто
+            //непонимаю почему не срабатывет программа верно
+            //string[] files = Directory.GetFiles(path, "", SearchOption.AllDirectories);
+
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ _8/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+/*Программно создайте текстовый файл и запишите в него 10 
+случайных чисел. Затем программно откройте созданный файл, 
+рассчитайте сумму чисел в нем, ответ выведите на консоль.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Test\Test.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    sw.WriteLine(random.Next(-5, 5));
+                }
+            }
+            int sum = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sum = sum + Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
